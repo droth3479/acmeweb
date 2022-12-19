a90881508b8488162e3609d8d04e4d84baec8fd5
droth3479 <92330553+droth3479@users.noreply.github.com>
2022-12-19 14:25:29 -0500

    addressed issue of name parameter not working properly in detailed url.

--- src/main/java/com/acme/statusmgr/Decorators/StatusDecorator.java
+++ src/main/java/com/acme/statusmgr/Decorators/StatusDecorator.java
@@ -2,11 +2,21 @@ package com.acme.statusmgr.Decorators;
 
 import com.acme.statusmgr.beans.AbstractServerStatus;
 
+/**
+ * Abstract class to serve as the super class for all decorators
+ * <p>
+ * Extends AbstractServerStatus, and serves as a wrapper for an AbstractServerStatus object
+ */
 abstract class StatusDecorator extends AbstractServerStatus {
     private final AbstractServerStatus statusToBeDecorated;
 
     public StatusDecorator(AbstractServerStatus statusToBeDecorated) {
         this.statusToBeDecorated = statusToBeDecorated;
+    }
+
+    @Override
+    public String getContentHeader() {
+        return statusToBeDecorated.getContentHeader();
     }
 
     @Override
